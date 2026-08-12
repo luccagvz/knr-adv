5b10c3906a467bda26f43de7d453d7c063a01de1
Lucca Gavazza <dev.lucca.gavazza@gmail.com>
2026-08-12 14:46:25 -0300

    Adiciona favicon com a marca KNR
    Recorta o monograma "KNR" do logo existente pra usar como ícone da
    aba do navegador (site público e /admin), no lugar do globo genérico
    padrão. Inclui apple-touch-icon pra quando alguém adicionar o site à
    tela inicial do celular.

--- site/components/seoHead.js
+++ site/components/seoHead.js
@@ -14,6 +14,9 @@ module.exports = function seoHead({ title, description, path, ogTitle, ogDescrip
     <title>${fullTitle}</title>
     <meta name="description" content="${description || ''}">
     <link rel="canonical" href="${canonical}">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
     <meta property="og:type" content="${ogType}">
     <meta property="og:title" content="${resolvedOgTitle}">
     <meta property="og:description" content="${resolvedOgDescription}">
